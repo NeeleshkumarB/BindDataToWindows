cf36ee50c2f66332c7e42c45b8c81550795067ef
Neelesh <bodgalneelesh@gmail.com>
2020-05-22 23:14:08 +0530

    Initial Commit

---
+++ BindDataToWindows/BindDataToWindows/DataPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindDataToWindows
+{
+    class DataPoint : INotifyPropertyChanged
+    {
+
+        private string _value;
+
+        public string Value
+        {
+            get { return _value; }
+            set { _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+
+        #region INotifyPropertyChanged Members
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        #endregion
+    }
+}
